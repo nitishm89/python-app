048c275ecb7de6759a0c0f6df0e67f22bacf6fe1
Rasika <rasikapande3008@gmail.com>
2026-07-27 13:03:07 +0530

    commit

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'Hello Shrinay from Daddy & Mom <3',
+        'message': 'Hello Shrinay from Daddy & Mom <3 ,Love you',
         'deployed_on': 'kubernetes'
     })
 
